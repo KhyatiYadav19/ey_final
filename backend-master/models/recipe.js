383bcad4ccfa462c3e4145f68aec65b1ca01de99
KhyatiYadav19 <118345300+KhyatiYadav19@users.noreply.github.com>
2025-09-28 01:35:49 +0530

    Update recipe.js

--- backend-master/models/recipe.js
+++ backend-master/models/recipe.js
@@ -19,10 +19,10 @@ const recipeSchema=mongoose.Schema({
     coverImage:{
         type:String,
     },
-    // createdBy:{
-    //     type:mongoose.Schema.Types.ObjectId,
-    //     ref:"user"
-    // }
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
 
 },{timestamps:true})
 

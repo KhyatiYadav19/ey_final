3a1713419285b5ba3528a13e057f311d32f3938c
KhyatiYadav19 <118345300+KhyatiYadav19@users.noreply.github.com>
2025-09-28 01:45:23 +0530

    Update recipe.js

--- backend-master/routes/recipe.js
+++ backend-master/routes/recipe.js
@@ -1,12 +1,13 @@
 const express=require("express")
-const { getRecipes,getRecipe,addRecipe,editRecipe,deleteRecipe, upload} = require("../controller/recipe")
-// const verifyToken = require("../middleware/auth")
+const { getRecipes,getRecipe,addRecipe,editRecipe,deleteRecipe, getMyRecipes, upload} = require("../controller/recipe")
+const verifyToken = require("../middleware/auth")
 const router=express.Router()
 
 router.get("/",getRecipes) //Get all recipes
+router.get("/my", verifyToken, getMyRecipes) //Get my recipes
 router.get("/:id",getRecipe) //Get recipe by id
-router.post('/', upload.single('file'), addRecipe) //add recipe
+router.post('/', verifyToken, upload.single('file'), addRecipe) //add recipe
 router.put("/:id",upload.single('file'),editRecipe) //Edit recipe
-router.delete("/:id",deleteRecipe) //Delete recipe
+router.delete("/:id", verifyToken, deleteRecipe) //Delete recipe
 
 module.exports=router
